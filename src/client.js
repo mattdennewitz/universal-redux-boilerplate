08740e4968ef3fc668b1bc98492b1653d27240ea
Matt Dennewitz <mattdennewitz@gmail.com>
2015-09-21 15:17:51 -0500

    cleanup

--- src/client.js
+++ src/client.js
@@ -5,16 +5,17 @@ import { Provider } from 'react-redux';
 import { ReduxRouter } from 'redux-router';
 import createBrowserHistory from 'history/lib/createBrowserHistory';
 
+import configureStore from './store';
 import routes from './routes';
 
 /* create store */
-var configureStore = require('./store');
-var store = configureStore('client', window.__initial_state__);
+const store = configureStore('client', window.__initial_state__);
 
-var Root = (
+const Root = (
     <Provider store={store}>
         {() => <ReduxRouter routes={routes} />}
     </Provider>
 );
 
+/* boot react/redux/etc on the client */
 React.render(Root, document.getElementById('root'));
